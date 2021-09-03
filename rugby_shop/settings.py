59ab6bf04afe98629e75008cbdf25dad53332359
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-09-03 16:17:05 +0000

    feat: Add home app and templates

--- rugby_shop/settings.py
+++ rugby_shop/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -82,7 +83,10 @@ ROOT_URLCONF = 'rugby_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
