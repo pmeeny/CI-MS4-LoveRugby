efc0569654b276c76742bbaed0d75044be6f5f80
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-09-06 09:25:59 +0000

    feat: Add static and media directories

--- rugby_shop/settings.py
+++ rugby_shop/settings.py
@@ -155,3 +155,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
