85535cd471a8a84348f8dc1c1f6230208d7c0656
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-09-22 10:33:58 +0000

    feat: Add fields to order

--- checkout/models.py
+++ checkout/models.py
@@ -22,6 +22,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
@@ -71,4 +73,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.code} on order {self.order.order_number}'
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
