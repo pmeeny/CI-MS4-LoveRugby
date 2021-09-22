74f13b6b4e36e6af8e9e36f1e4fa0d4e249bbc3f
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-09-22 10:33:48 +0000

    feat: Add fields to order

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,4 +1,4 @@
-
+   
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
@@ -13,13 +13,15 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
