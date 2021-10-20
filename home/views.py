d2342872acad203cef13580e6a55705c15b79e01
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-10-20 12:59:32 +0000

    refactor: Pass in products obj to index

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
-
+    products = Product.objects.all()
+    return render(request, 'home/index.html', dict(products=products))
