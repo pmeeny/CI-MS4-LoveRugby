622fbd6c527600e80e3f26645110f8c3d82a6595
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-10-20 16:15:14 +0000

    feat: Add features, has_sizes, new_release

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
 class Category(models.Model):
 
@@ -23,11 +24,14 @@ class Product(models.Model):
     colour = models.CharField(max_length=254)
     code = models.CharField(max_length=254)
     description = models.TextField()
-    features = models.CharField(max_length=254)
-    size = models.CharField(max_length=254)
+    feature1 = models.CharField(max_length=254, blank=True)
+    feature2 = models.CharField(max_length=254, blank=True)
+    feature3 = models.CharField(max_length=254, blank=True)
+    feature4 = models.CharField(max_length=254, blank=True) 
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     pre_sale_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    new_release = models.CharField(max_length=3, null=True, blank=True)
+    new_release = models.BooleanField(default=False)   
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     
