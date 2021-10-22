c6fd19bce77e3c9682673343eed6d9f181adf982
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-10-22 16:06:32 +0100

    refactor: Add 127.0.01 to allowed hosts

--- rugby_shop/settings.py
+++ rugby_shop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ci-ms4-recordshop.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ci-ms4-recordshop.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
