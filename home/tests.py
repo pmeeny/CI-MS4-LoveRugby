2914549189ece48d42646d69e4a650ad1a58b126
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-10-22 20:44:42 +0100

    feat: Add comment to test case

--- home/tests.py
+++ home/tests.py
@@ -4,6 +4,10 @@ from django.test import TestCase
 class TestHomeViews(TestCase):
 
     def test_get_home_page(self):
+        """
+        This test checks that the index page
+        is displayed
+        """
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
