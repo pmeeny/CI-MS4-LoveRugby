05223fa28a60b6a49c4c0fdf2cc30ce4319331e5
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-10-25 21:33:40 +0100

    refactor: Pep8 errors fixed

--- bag/views.py
+++ bag/views.py
@@ -3,12 +3,12 @@ from django.contrib import messages
 
 from products.models import Product
 
-# Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
