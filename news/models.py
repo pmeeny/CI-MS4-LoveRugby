ddc9a2cd47c335e43772a4dd4991f8271f402a05
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-10-26 11:35:16 +0100

    feat: Add models and test_models unit tests

---
+++ news/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Published")
+)
+
+
+class News(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='news_items')
+    news_item_text = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    update_date = models.DateTimeField(auto_now=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-create_date']
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    new_story = models.ForeignKey(
+        News, on_delete=models.CASCADE, related_name="comments")
+    comment_text = models.TextField(null=False, blank=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['create_date']
+
+    def __str__(self):
+        return self.comment_text
+
---
+++ news/test_models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from news.models import News, Comment
+
+
+class TestNewsModels(TestCase):
+    def setUp(self):
+
+        test_user = User.objects.create_user(
+            username='test_user', password='test_password')
+
+        test_news_story = News.objects.create(
+            title='Test Title',
+            user=test_user,
+            news_item_text='Test Item Text',
+            image = 'TestImage.png',
+            update_date = '01/01/2021',
+            create_date = '01/01/2021',
+            status = 1,
+        )
+
+        Comment.objects.create(
+            user=test_user,
+            new_story=test_news_story,
+            comment_text = 'Test Item Text',
+            create_date = '01/01/2021',
+        )
+
+    def test_news_str_method(self):
+        """
+        This test tests the news str method
+        """
+        news_item = News.objects.get(title='Test Title')
+        self.assertEqual((news_item.__str__()), news_item.title)
+
+    def test_comment_str_method(self):
+        """
+        This test tests the news str method
+        """
+        comment = Comment.objects.get(comment_text='Test Item Text')
+        self.assertEqual((comment.__str__()), comment.comment_text)
