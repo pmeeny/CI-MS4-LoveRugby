c0d8daeda476a7b5e80806c530946ebfe474b66d
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-10-29 17:08:27 +0100

    refactor: Update allowed host typo

--- rugby_shop/settings.py
+++ rugby_shop/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ci-ms4-loverugby.herokuapp.com/', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['ci-ms4-loverugby.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
