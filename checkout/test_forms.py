5388207b3ef2fd4056127b9c8202fc528906e794
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-11-01 17:33:38 +0000

    feat: Add one unit test for forms

--- checkout/test_forms.py
+++ checkout/test_forms.py
@@ -1,21 +1,21 @@
 from django.test import TestCase
 
 from .forms import OrderForm
-from .models import Order
 
 
 class TestCheckoutForms(TestCase):
 
     def test_add_order_form(self):
+        """
+        This test tests the order form object
+        """
         form = OrderForm({
-            'full_name': 'Test Name',
-            'email': 'test@gmail.com',
-            'phone_number': '123456789',
-            'street_address1': 'Test Address 1',
-
+            'full_name': 'test name',
+            'email': 'test@email.com',
+            'phone_number': '123456',
+            'country': 'IE',
+            'town_or_city': 'test city',
+            'street_address1': 'test address 1',
+            'street_address2': 'test address 2',
             })
         self.assertTrue(form.is_valid())
-        form.save()
-        Order.objects.get(full_name='Test Name')
-        Order.objects.get(email='test@gmail.com')
-        Order.objects.get(country='Test Country')
