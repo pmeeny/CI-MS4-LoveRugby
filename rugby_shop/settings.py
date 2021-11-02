69dc59e65f6a2b2cac4712665ff62b755e2eb8f2
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-11-02 12:30:01 +0000

    feat: Pep8online fixes

--- rugby_shop/settings.py
+++ rugby_shop/settings.py
@@ -82,7 +82,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
