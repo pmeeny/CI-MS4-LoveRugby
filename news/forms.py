65ed580998db0e5da6e4292f4eaa4290cb810d3b
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-11-04 16:04:16 +0000

    refactor: Update to form label

--- news/forms.py
+++ news/forms.py
@@ -20,16 +20,21 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('comment_text',)
+        labels = {
+            'comment_text': '',
+        }
 
     def __init__(self, *args, **kwargs):
         """ Add placeholder and remove auto-generated labels """
         super().__init__(*args, **kwargs)
         placeholders = {
-            'comment_text': 'Add Comment',
+            'comment_text': 'Add your comment text here and click the Post '
+                            'Comment button',
         }
 
         for field in self.fields:
             placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'border-black rounded-1'
-            self.fields[field].label = True
+            self.fields[field].label = "Comment "
+
