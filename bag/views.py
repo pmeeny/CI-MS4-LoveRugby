93dec760fbd1529040f0eadf5a0272b9222be1ab
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-11-08 13:44:29 +0000

    refactor: Pep8 fixes

--- bag/views.py
+++ bag/views.py
@@ -18,6 +18,7 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
+
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
@@ -60,6 +61,7 @@ def adjust_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
+
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
