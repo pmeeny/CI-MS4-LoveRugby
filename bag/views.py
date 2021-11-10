585ee1b5c8dd45f5191f0af6991b62697e8708f8
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-11-10 12:39:13 +0000

    refactor: Improved comment text on each method

--- bag/views.py
+++ bag/views.py
@@ -12,14 +12,17 @@ from products.models import Product
 
 
 def view_bag(request):
-    """ A view that renders the bag contents page """
+    """
+    A view that renders the bag contents page
+    """
 
     return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
-
+    """
+    Add a quantity of the specified product to the shopping bag
+    """
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -62,8 +65,9 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    """Adjust the quantity of the specified product to the specified amount"""
-
+    """
+    Adjust the quantity of the specified product to the specified amount
+    """
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -101,8 +105,9 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
-
+    """
+    Remove the item from the shopping bag
+    """
     try:
         product = get_object_or_404(Product, pk=item_id)
         size = None
